c6b5e119f341f84828a19d2cddf4758b95435c04
David Knaack <davidkna@users.noreply.github.com>
2021-02-10 13:46:45 +0100

    visualisierung: increase font size

--- Visualisierung/main.py
+++ Visualisierung/main.py
@@ -6,6 +6,22 @@ import os
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("The quick brown fox jumps over the lazy dog.")
 svg = displacy.render(doc, style="dep", jupyter=False)
+
+STYLE = """
+<style>
+.displacy-word, .displacy-label {
+    font-size: 2em;
+}
+
+.displacy-tag {
+    font-size: 1.4em;
+}
+</style>
+</svg>
+"""
+
+svg = svg.replace("</svg>", STYLE)
+
 with Path("demo_dep.svg").open("w", encoding="utf-8") as f:
     f.write(svg)
 
